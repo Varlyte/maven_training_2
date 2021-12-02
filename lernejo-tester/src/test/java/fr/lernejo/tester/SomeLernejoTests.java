7dde410984245e01786615ee481dae413b08bcce
Lucas Mari <mari@et.esiea.fr>
2021-12-02 10:30:04 +0100

    test added

---
+++ lernejo-tester/src/test/java/fr/lernejo/tester/SomeLernejoTests.java
@@ -0,0 +1,13 @@
+package fr.lernejo.tester;
+
+import fr.lernejo.tester.api.TestMethod;
+
+public class SomeLernejoTests {
+    @TestMethod
+    public void ok(){}
+    @TestMethod
+    public void ko() {
+        new java.lang.IllegalStateException();
+    }
+    public void none() {}
+}
